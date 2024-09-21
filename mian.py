f91d10f6f21353f6814afda5becd7f38994399b6
Techystark <techystarkcontact@gmail.com>
2024-09-21 18:25:09 +0530

    commit

---
+++ mian.py
@@ -0,0 +1,19 @@
+number = int(input("enter First Number : "))
+opeator = input("enter opeator : ")
+number2 = int(input("Enter Second NUmber :"))
+
+
+if opeator == '+':
+    print(number + number2)
+elif opeator == '-':
+    print(number-number2)
+elif opeator == '*':
+    print(number * number2)
+elif opeator == '/':
+    print(number / number2)
+else:
+    print("invalid Input !!!")
+
+
+
+    
